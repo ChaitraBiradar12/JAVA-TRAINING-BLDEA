385ea578564369f3cf9a887f96377fb757525b23
cbb1234 <biradarchaitra376@gmail.com>
2023-03-22 17:22:13 +0530

    first commit

---
+++ src/main/java/testingcodes/testCode.java
@@ -0,0 +1,10 @@
+package testingcodes;
+
+public class testCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("chaitra biradar");
+	}
+
+}
